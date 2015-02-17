2cf8487d98da4971b02de2afa3b4ed0590828813
Cameron Webster <cameron.webster@gmail.com>
2015-02-17 13:45:56 -0700

    user can only see clubs they are a member of

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,12 +1,16 @@
 class ClubsController < ApplicationController
   before_filter :authorize, only: [:index, :show]
-  
+
   def index
-    @clubs = Club.all
+    @clubs = current_student.clubs
   end
 
   def show
     @club = Club.find(params[:id])
+
+    if !@club.students.find_by_id(current_student.id)
+      render 'public/403.html'
+    end
   end
 
 end
